6138f4052c74a5af586ee2113d71d0791f9ac12a
Edu Olivares <116183221+eduolihez@users.noreply.github.com>
2025-01-28 12:14:00 +0100

    Dia 1 | Teoria Acabada

--- Dia_1/operadores/Operadores_de_Asignacion.py
+++ Dia_1/operadores/Operadores_de_Asignacion.py
@@ -9,4 +9,34 @@ Operadores de Asignación
 //= : División entera y asigna (x //= 2 equivale a x = x // 2).
 """
 
-print(2=2)
+# Uso del Operador =
+a = 10
+b = 20
+c = 30
+d = 40
+e = 50
+f = 60
+
+# Uso del operador +=
+a += 5
+print(a)
+
+# Uso del operador -=
+b -= 5
+print(b)
+
+# Uso del operador *=
+c *= 3
+print(c)
+
+# Uso del operador /=
+d /= 20
+print(d)
+
+# Uso del operador %=
+e %= 20
+print(e)
+
+# Uso del operador //=
+f //= 15
+print(f)
